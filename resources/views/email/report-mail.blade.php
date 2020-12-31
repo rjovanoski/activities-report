87c4c7f0cf13d5389f05dfec64fa38e92e775cbf
rjovanoski <rade_j@hotmail.com>
2020-12-31 20:53:46 +0100

    Updated report link

--- resources/views/email/report-mail.blade.php
+++ resources/views/email/report-mail.blade.php
@@ -8,8 +8,8 @@ Report
 @endcomponent
 
 
-If you can't enter through the button please copy this link
-http://localhost:8000/home/report/{{ $token }}
+If you're having trouble clicking the "Report" button, copy and paste the URL below into your browser: 
+<a href="http://localhost:8000/home/report/{{ $token }}">http://localhost:8000/home/report/{{ $token }}</a>
 
 Thanks,<br>
 {{ config('app.name') }}
